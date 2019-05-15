858cd2cb72ddf6205dd8b1f3f9e943c2350f7b92
nx1 <norman1000@gmail.com>
2019-05-15 10:40:14 +0100

    Create cat.py

---
+++ cat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 15 10:39:19 2019
+
+@author: nk7g14
+"""
+
+class ULX():
+    def __init__(name, ra, dec):
+        self.name = name
+        self.ra = ra
+        self.dec = dec
